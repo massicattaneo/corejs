8b90dc158634b2f80fd5d190997a1fae090469c4
mcattaneo <mcattaneo@riplife.es>
2016-05-16 10:31:39 +0200

    ut improvements

--- tasks/unit-tests.js
+++ tasks/unit-tests.js
@@ -1,19 +1,29 @@
 var config = require('../gulp-config.js');
 var karma = require('karma');
 var gulp = require('gulp');
+var args = require('get-gulp-args')();
 
 gulp.task('ut', function (done) {
+
+    var files = config.tests.concat(config.files);
+    var preprocessors = {
+        'js/**/!(*Spec).js': ['coverage']
+    };
+
+    if (args.test) {
+        files = ['js/**/'+args.test+'Spec.js'].concat(config.files);
+        preprocessors = {};
+        preprocessors['js/**/'+args.test+'.js'] = ['coverage'];
+    }
 
     var server = new karma.Server({
         configFile: __dirname + '/../karma.conf.js',
         action: 'run',
         singleRun: false,
         autoWatch: true,
-        files: config.tests.concat(config.files),
+        files: files,
         reporters: ['mocha', 'coverage'],
-        preprocessors: {
-            'js/**/!(*Spec).js': ['coverage']
-        },
+        preprocessors: preprocessors,
         coverageReporter: {
             type: 'text',
             dir: 'coverage/',
