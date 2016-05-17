9de6f44d24b788cc49c01568c6ffe791890c9a3a
mcattaneo <mcattaneo@riplife.es>
2016-05-17 16:50:47 +0200

    Component

--- js/Component/Component.js
+++ js/Component/Component.js
@@ -13,18 +13,60 @@ var Component = function () {
 
     var components = [];
 
-    var createNode = function (template) {
-        var n = document.createElement('div');
-        n.innerHTML = template;
-        return n.firstChild;
+    var createNode = function (markup) {
+        var doc = document.implementation.createHTMLDocument("");
+        doc.body.innerHTML = markup;
+        return doc.body.childNodes[0];
+    };
+
+    function createListeners(attribute, node, obj) {
+        var split = attribute.trim().split(':');
+        var actions = split[0].split(',');
+        var listener = split[1];
+        actions.forEach(function (action) {
+            node.addListener(action, function (event) {
+                obj[listener](event);
+            });
+        })
+    }
+
+    var createItems = function (attribute, node, obj) {
+        obj.items.add(node, attribute);
+    };
+
+    var attach = function (node, obj, attrName) {
+        if (node.getAttribute) {
+            var attribute = node.getAttribute(attrName);
+            if (attribute) {
+                attrName === 'data-on' && createListeners(attribute, node, obj);
+                attrName === 'data-item' && createItems(attribute, node, obj);
+            }
+        }
+    };
+
+    var parseNode = function (node, obj) {
+        attach(node, obj, 'data-on');
+        attach(node, obj, 'data-item');
+        var nodes = Array.prototype.slice.call(node.childNodes);
+        nodes.forEach(function (n) {
+            parseNode(n, obj);
+        });
     };
 
     var Component = function (template) {
         var node = createNode(template);
-        var nameMatch = template.match(/data-name="(.*)"/);
 
         var obj = {
-            name: nameMatch ? nameMatch[1] : ''
+            items: Collection()
+        };
+
+        obj.createIn = function (parent) {
+            parent.appendChild(node);
+            node && parseNode(node, obj);
+        };
+
+        obj.get = function (itemName) {
+            return obj.items.get(itemName);
         };
 
         return obj;
--- js/Component/ComponentSpec.js
+++ js/Component/ComponentSpec.js
@@ -6,23 +6,39 @@ describe('GLOBAL - COMPONENT', function () {
 
     describe('On having a HTML template', function () {
 
-        it('should get the name to the component', function () {
-            var test = Component('<div data-name="test"></div>');
-            expect(test.name).toEqual('test');
+        it('should attach listeners', function () {
+            var test = Component('<div><button id="button1" data-on="click:click1"></button><button id="button2" data-on="click:click2"></button></div>');
+            test.createIn(document.body);
+
+            test.click1 = function () {};
+            test.click2 = function () {};
+            spyOn(test, 'click1');
+            spyOn(test, 'click2');
+
+            $('#button1').click();
+            expect(test.click1).toHaveBeenCalled();
+            expect(test.click2).not.toHaveBeenCalled();
+            $('#button2').click();
+            expect(test.click2).toHaveBeenCalled();
         });
 
-        it('should attach listeners', function () {
-            var test = Component('<div id="a" data-name="test"><button id="button1" data-on="click:click1"></button><button id="button2" data-on="click:click2"></button></div>');
-                test.click1 = function () {};
-                test.click2 = function () {};
-                spyOn(test, 'click1');
-                spyOn(test, 'click2');
-                $('#button1').click();
-                $('#button2').click();
-                expect(test.click1).toHaveBeenCalled();
-                // expect(test.click2).not.toHaveBeenCalled();
-                // spyOn(test, 'click2');
-                // expect(test.click2).toHaveBeenCalled();
+        it('should create items', function () {
+            var test = Component('<input type="text" data-item="input" />');
+            test.createIn(document.body);
+            expect(test.get('input')).toBeDefined();
+        })
+
+    });
+
+    describe('On reusing components', function () {
+        var input;
+        
+        beforeEach(function () {
+            input = Component('<input data-item="input" type="text" /><span data-item="error"');
+        });
+
+        it('should replace the html', function () {
+            var c = Component('<div><utils:input/></div>');
         });
 
     })
