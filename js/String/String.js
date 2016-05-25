36173b3dfc22f9d92723093b907451f0efbfe05e
mcattaneo <mcattaneo@riplife.es>
2016-05-25 17:52:49 +0200

    String

--- js/String/String.js
+++ js/String/String.js
@@ -9,3 +9,17 @@
  //       Copyright (c) 2016.
  //////////////////////////////////////////////////////////////////////////////
  */
+
+ String.prototype.padLeft = function (size, char) {
+     if (size === 0) {
+         return '';
+     }
+     return (Array(size + 1).join(char) + this).slice(-size);
+ };
+
+ String.prototype.padRight = function (size, char) {
+     if (size === 0) {
+         return '';
+     }
+     return (this + Array(size + 1).join(char)).slice(0, size);
+ };
--- js/String/StringSpec.js
+++ js/String/StringSpec.js
@@ -1,5 +1,44 @@
 describe("PROTOTYPE - STRING", function () {
 
-    
+    it('padLeft', function() {
+        var str = '';
+        expect(str.padLeft(0, "0")).toBe('');
+        expect(str.padLeft(1, "0")).toBe('0');
+        expect(str.padLeft(2, "0")).toBe('00');
+        expect(str.padLeft(6, "0")).toBe('000000');
+
+        var str = '1';
+        expect(str.padLeft(0, "0")).toBe('');
+        expect(str.padLeft(1, "0")).toBe('1');
+        expect(str.padLeft(2, "0")).toBe('01');
+
+        var str = '123';
+        expect(str.padLeft(0, "0")).toBe('');
+        expect(str.padLeft(1, "0")).toBe('3');
+        expect(str.padLeft(2, "0")).toBe('23');
+        expect(str.padLeft(3, "0")).toBe('123');
+    });
+
+    it('padRight', function() {
+        var str = '';
+        expect(str.padRight(0, "0")).toBe('');
+        expect(str.padRight(1, "0")).toBe('0');
+        expect(str.padRight(2, "0")).toBe('00');
+        expect(str.padRight(6, "0")).toBe('000000');
+
+        str = '1';
+        expect(str.padRight(0, "0")).toBe('');
+        expect(str.padRight(1, "0")).toBe('1');
+        expect(str.padRight(2, "0")).toBe('10');
+        expect(str.padRight(4, "0")).toBe('1000');
+        expect(str.padRight(6, "0")).toBe('100000');
+
+        var str = '123';
+        expect(str.padRight(0, "0")).toBe('');
+        expect(str.padRight(1, "0")).toBe('1');
+        expect(str.padRight(2, "0")).toBe('12');
+        expect(str.padRight(3, "0")).toBe('123');
+        expect(str.padRight(5, "0")).toBe('12300');
+    });
 
 });
